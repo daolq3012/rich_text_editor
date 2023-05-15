71bf42e57ee0fc7dd5a348e54241d1ba38078c46
le.quang.dao <le.quang.dao@sun-asterisk.com>
2023-05-15 09:30:17 +0700

    Implement richtext editor

---
+++ example/lib/assets_image.dart
@@ -0,0 +1,8 @@
+class AssetsImage {
+  static const bold = 'packages/rich_text_editor/assets/images/ic_bold.svg';
+  static const italic = 'packages/rich_text_editor/assets/images/ic_italic.svg';
+  static const strikeThrough = 'packages/rich_text_editor/assets/images/ic_strike_through.svg';
+  static const underline = 'packages/rich_text_editor/assets/images/ic_underline.svg';
+  static const bulletList = 'packages/rich_text_editor/assets/images/ic_bullet_list.svg';
+  static const numberList = 'packages/rich_text_editor/assets/images/ic_number_list.svg';
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:rich_text_editor/rich_editor.dart';
+
+import 'rich_editor_toolbar.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,13 +18,15 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(title: 'Flutter Demo Rich Editor'),
       );
 }
 
+// create MyHomePage stateless widget class with title property
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  // final property title
   final String title;
 
   @override
@@ -28,14 +34,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late final GlobalKey<RichEditorState> _richEditorKey;
+  late final GlobalKey<RichEditorToolbarState> _richToolbarEditorKey;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _richEditorKey = GlobalKey<RichEditorState>();
   }
 
+  // build method
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -43,22 +51,43 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+            children: [
+              RichEditorToolbar(
+                key: _richToolbarEditorKey,
+                onTap: (EditorStyleType style) {
+                  // ignore: missing_enum_constant_in_switch
+                  switch (style) {
+                    case EditorStyleType.BOLD:
+                      _richEditorKey.currentState?.setBold();
+                      break;
+                    case EditorStyleType.ITALIC:
+                      _richEditorKey.currentState?.setItalic();
+                      break;
+                    case EditorStyleType.STRIKETHROUGH:
+                      _richEditorKey.currentState?.setStrikeThrough();
+                      break;
+                    case EditorStyleType.UNDERLINE:
+                      _richEditorKey.currentState?.setUnderline();
+                      break;
+                    case EditorStyleType.UNORDERED_LIST:
+                      _richEditorKey.currentState?.setBullets();
+                      break;
+                  }
+                },
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
+              SizedBox(
+                height: 100,
+                child: RichEditor(
+                  key: _richEditorKey,
+                  placeholder: 'Sample placeholder',
+                  onStyleTextFocused: (editorStyles) {
+                    _richToolbarEditorKey.currentState
+                        ?.updateStyle(editorStyles);
+                  },
+                ),
               ),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
       );
 }
